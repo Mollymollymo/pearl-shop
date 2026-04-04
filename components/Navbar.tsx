bad705e2923714d5a86f5e417894a625b0f8d59e
Molly <erdy521@gmail.com>
2026-04-04 14:31:30 +0800

    Navbar RWD

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,45 @@
-export default function Navbar() {
-  return (
-    <nav className="flex items-center justify-between px-6 py-4 bg-[#C9A9B3] text-white shadow-sm">
-      <h1 className="text-xl font-semibold">Pearl Shop</h1>
+"use client";
 
-      <div className="flex gap-3">
-        <div className="p-2">Home</div>
-        <div className="p-2">Products</div>
-        <button className="px-4 py-2 bg-white text-[#C9A9B3] rounded-md">
-          Login
+import Link from "next/link";
+import { useState } from "react";
+
+export default function Navbar() {
+
+  const[ isOpen, setIsOpen ] = useState(false);
+
+  const menuItems = (
+    <>
+      <Link href="/" className="p-2">Home</Link>
+      <Link href="/products" className="p-2">Products</Link>
+      <Link href="/cart" className="p-2">Cart</Link>
+      <button className="px-4 py-2 bg-white text-[#C9A9B3] rounded-md">
+        Login
+      </button>
+    </>
+  )
+  return (
+    <nav className="bg-[#C9A9B3] text-white shadow-sm px-6 py-4">
+      <div className="flex justify-between items-center">
+        <h1 className="text-xl font-semibold">Pearl Shop</h1>
+
+        <button
+          className="md:hidden"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          ☰
         </button>
+
+        <div className="hidden md:flex gap-3 items-center">
+          {menuItems}
+        </div>
       </div>
+
+      {isOpen && (
+        <div className="flex flex-col mt-2 md:hidden gap-2">
+          {menuItems}
+        </div>
+      )}
     </nav>
   );
+  
 }
